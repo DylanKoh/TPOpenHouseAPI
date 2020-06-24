6e0d8e2161b88d7a4cc4fdb67010bcb41b81b947
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-06-24 14:07:19 +0800

    Reimported dB

--- TPOpenHouseModel.Designer.cs
+++ TPOpenHouseModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Users\Dylan Koh\source\repos\TPOpenHouseAPI\TPOpehHouseModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Users\Dylan Koh\source\repos\TPOpenHouseAPI\TPOpenHouseModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
